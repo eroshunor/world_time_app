5e93706deaab1d5fb4ec8a7b362fd482f8a2f6b0
eroshunor <51173183+eroshunor@users.noreply.github.com>
2022-03-10 17:10:27 +0200

    async code added
    async code added

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/services/world_time.dart';
 
 class LoadingPage extends StatefulWidget {
   const LoadingPage({Key? key}) : super(key: key);
@@ -9,15 +10,26 @@ class LoadingPage extends StatefulWidget {
 
 class _LoadingPageState extends State<LoadingPage> {
 
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'europe/Berlin');
+    await instance.getTime();
+    setState(() {
+      time = instance.time;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    setupWorldTime();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('LoadingPage'),
+      body: Text(time),
     );
   }
 }
