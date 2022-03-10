234e16b0dfd5fe0f02138dfbfc3708878f522fd6
eroshunor <51173183+eroshunor@users.noreply.github.com>
2022-03-10 11:56:03 +0200

    pages added
    required screens added to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/home_page.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(MaterialApp(
+  home: HomePage(),
+  
+));
 
---
+++ lib/pages/choose_location_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocationPage extends StatefulWidget {
+  const ChooseLocationPage({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseLocationPage> createState() => _ChooseLocationPageState();
+}
+
+class _ChooseLocationPageState extends State<ChooseLocationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('ChooseLocationPage'),
+    );
+  }
+}
