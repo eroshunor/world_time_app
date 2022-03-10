a9c798eb0fb0ca9bff929fa44968433dbca98380
eroshunor <51173183+eroshunor@users.noreply.github.com>
2022-03-10 12:01:33 +0200

    routing added
    routing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/choose_location_page.dart';
 import 'package:world_time_app/pages/home_page.dart';
+import 'package:world_time_app/pages/loading_page.dart';
 
 void main() => runApp(MaterialApp(
-  home: HomePage(),
-  
+  initialRoute: '/homePage',
+  routes: {
+    '/': (context) => LoadingPage(),
+    '/homePage' : (context) => HomePage(),
+    '/chooseLocationPage': (context) => ChooseLocationPage()
+  },
 ));
 
